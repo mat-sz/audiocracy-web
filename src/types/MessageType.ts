30bc01406d2a1cb09aa1c1ae72bcac11457e0a2f
Mat Sz <contact@matsz.dev>
2020-12-05 13:20:48 +0100

    added message models

---
+++ src/types/MessageType.ts
@@ -0,0 +1,12 @@
+export enum MessageType {
+  // Received:
+  STATE = 'state',
+  MESSAGE = 'message',
+  DOWNVOTES = 'downvotes',
+  TIME = 'time',
+
+  // Sent:
+  DOWNVOTE = 'downvote',
+  ADD = 'add',
+  SKIP = 'skip',
+}
--- src/types/Models.ts
+++ src/types/Models.ts
@@ -1,8 +1,70 @@
 import { ActionType } from './ActionType';
+import { MessageType } from './MessageType';
 
 export interface ActionModel {
   type: ActionType;
   value: any;
 }
 
-export interface MessageModel {}
+export interface MessageModel {
+  type: MessageType;
+}
+
+export interface StateMessageModel extends MessageModel, ServerState {
+  type: MessageType.STATE;
+}
+
+export interface MessageMessageModel extends MessageModel {
+  type: MessageType.MESSAGE;
+  text: string;
+}
+
+export interface DownvotesMessageModel extends MessageModel {
+  type: MessageType.DOWNVOTES;
+  count: number;
+}
+
+export interface TimeMessageModel extends MessageModel {
+  type: MessageType.TIME;
+  time: number;
+}
+
+export interface AddMessageModel extends MessageModel {
+  type: MessageType.ADD;
+  url: string;
+}
+
+export interface DownvoteMessageModel extends MessageModel {
+  type: MessageType.DOWNVOTE;
+}
+
+export interface SkipMessageModel extends MessageModel {
+  type: MessageType.SKIP;
+  password: string;
+}
+
+export type Message =
+  | StateMessageModel
+  | MessageMessageModel
+  | DownvotesMessageModel
+  | TimeMessageModel
+  | AddMessageModel
+  | DownvoteMessageModel
+  | SkipMessageModel;
+
+export interface QueueItem {
+  id: string;
+  url?: string;
+  stream: string;
+  title: string;
+  author?: string;
+  thumbnail?: string;
+  duration: number;
+  website: string;
+}
+
+export interface ServerState {
+  queue: QueueItem[];
+  current: QueueItem;
+  downvotes: number;
+}
