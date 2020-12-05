b4d539cff1cc198474814ace77f5d5d5c0ec1b75
Mat Sz <contact@matsz.dev>
2020-12-05 13:08:32 +0100

    added a WebSocket connection

--- src/store.ts
+++ src/store.ts
@@ -3,10 +3,16 @@ import createSagaMiddleware from 'redux-saga';
 
 import { applicationState, StoreType } from './reducers';
 import { root } from './sagas';
+import { socketMiddleware } from './socketMiddleware';
+
+const serverUrl = process.env.REACT_APP_SERVER_URL as string;
 
 export const newStore = (): StoreType => {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(applicationState, applyMiddleware(sagaMiddleware));
+  const store = createStore(
+    applicationState,
+    applyMiddleware(socketMiddleware(serverUrl), sagaMiddleware)
+  );
 
   sagaMiddleware.run(root);
 
